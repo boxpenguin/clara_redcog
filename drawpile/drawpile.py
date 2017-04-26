ec55b6e3317eb1a86321ed556aab6816fb4981f7
UEHARA <juehara@gs6151-isfsadev.ndc.nasa.gov>
2017-04-26 10:33:43 -0400

    Comments for v1

--- drawpile/drawpile.py
+++ drawpile/drawpile.py
@@ -1,127 +1,52 @@
 #/usr/bin/python3.5
-import discord, os, collections, math, re, requests, json
+import discord, os, math, re, requests, json, time
 from discord.ext import commands
-from requests.auth import HTTPBasicAuth
 
-# Pre bot command funcs
+# Pre bot defs
 def convert_size(size_bytes):
-   if (size_bytes == 0):
+    """ Convert bytes to other IS values
+    From: http://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
+    @James Sapam """
+    if (size_bytes == 0):
        return '0B'
-   size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-   i = int(math.floor(math.log(size_bytes, 1024)))
-   p = math.pow(1024, i)
-   s = round(size_bytes/p, 2)
-   return '%s %s' % (s, size_name[i])
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes/p, 2)
+    return '%s %s' % (s, size_name[i])
 def getdata(type):
+    """ Access drawpile web admin JSON """
     url = ("http://localhost:8081/{0}/" .format(type))
-    output = requests.get(url, auth=HTTPBasicAuth('clara', 'amdk6-2')).text # displays content
+    output = requests.get(url).text
     jsonoutout = json.loads(output)
     return output
 def getip(ip):
+    """ Using the awesome ipinfo to get ip data and related information """
     url = ("https://ipinfo.io/{0}/" .format(ip))
-    output = requests.get(url).text # displays content
+    output = requests.get(url).text
     return output
-
 def gettime(lat,lng):
+    """ Using geonames to grab time and date from the lat/lng in conjunction of getip """
     url = ("http://api.geonames.org/timezoneJSON?lat={0}&lng={1}&username=demo" .format(lat, lng))
     output = requests.get(url).text
     return output
 
 class Drawpile:
+    """
+    Version 1.0 (20170426)
+    @Rakunko, Jordan Uehara
+    Red-bot custom cog
+    """
     def __init__(self, bot):
         self.bot = bot
-    """
-    What does this doo
-    Using drawpiles admin webserver I can grab information however this is poorly documented
-    and is not very clear for any platform. I will assume this means using any method to post
-    to a http server will do the  trick
-    https://github.com/drawpile/Drawpile/b/rutorrent/lob/master/doc/server-api.md
-    Commands:
-            drawpilegetsessions
-                Returns all sessions running
-                    GET /sessions/
-                    [
-                        {
-                            "id": "uuid (unique session ID)",
-                            "alias": "ID alias (if set)",
-                            //"protocol": "session protocol version",
-                            "userCount": "number of users",
-                            //"founder": "name of the user who created the session",
-                            "title": "session title",
-                            //"hasPassword": true/false (is the session password protected),
-                            //"closed": true/false (is the session closed to new users),
-                            "persistent": true/false (is persistence enabled for this session),
-                            "nsfm": true/false (does this session contain NSFM content),
-                            "startTime": "timestamp",
-                            "size": history size in bytes,
-                        }, ...
-                    ]
-            drawpilegetusersadmin
-                Returns all users all information
-                    GET /users/
-                    [
-                        {
-                            "session": "session ID (if empty, this user hasn't joined any session yet)",
-                                translate this ID to title
-                            "id": user ID (unique only within the session),
-                            "name": "user name",
-                            "ip": "IP address",
-                            "auth": true/false (is authenticated),
-                            "op": true/false (is session owner),
-                            //"muted": true/false (is blocked from chat),
-                            //"mod": true/false (is a moderator),
-                            //"tls": true/false (is using a secure connection)
-                        }
-            drawpilegetusers
-                Returns all users does not show ip
-                    GET /users/
-                    [
-                        {
-                            "session": "session ID (if empty, this user hasn't joined any session yet)",
-                                translate this ID to title
-                            //"id": user ID (unique only within the session),
-                            "name": "user name",
-                            //"ip": "IP address",
-                            //"auth": true/false (is authenticated),
-                            //"op": true/false (is session owner),
-                            //"muted": true/false (is blocked from chat),
-                            //"mod": true/false (is a moderator),
-                            //"tls": true/false (is using a secure connection)
-                        }
-                    ]
-            drawpilesendmsg session "message" #might not be needed
-                Send message to drawpile chat
-                PUT /session/:id/
-                {
-                    "message": "send a message to all participants",
-                    //"alert": "send an alert to all participants"
-                }
-            drawpilesendalert session "alert" #might not be needed
-                Send alert to drawpile chat
-                PUT /session/:id/
-                {
-                    //"message": "send a message to all participants",
-                    "alert": "send an alert to all participants"
-                }
-            drawpilesendspam  "alert"
-                Send alert to every session (for server alerts only)
-                PUT /session/:id/
-                {
-                    //"message": "send a message to all participants",
-                    "alert": "send an alert to all participants"
-                }
-    """
     @commands.command()
-    async def drawpiledetail(self, type):
+    async def drawpileadmin(self, type):
+        """ direct access to the admin JSON """
         await self.bot.say("Getting information")
         await self.bot.say(getdata(type))
-        # url = "http://localhost:8081/users/"
-        # output = requests.get(url, auth=HTTPBasicAuth('clara', 'amdk6-2')).text # displays content
-        # this did not work with the super complex php loader for wordpress but it can do some basic stuff
-        # might not be able handle the json out wont know til tomorrow sadly
-        # await self.bot.say("{0}" .format(output))
     @commands.command()
     async def drawpile(self):
+        """ Look up running sessions and logged in users """
         usercount = 0
         await self.bot.say("Getting Drawpile Session info :desktop:")
         session_json = getdata("sessions")
@@ -144,41 +69,41 @@ class Drawpile:
                 userip = users_data[b]['ip']
                 ip_json = getip(userip)
                 ip_data = json.loads(ip_json)
-                if ip_data['ip'] == "192.168.1.1":
+                if ip_data['ip'] == "192.168.1.1": # checks if user is on local network to the server
                     await self.bot.say("IP:\t\t\t\t\t localhost")
-                    # await self.bot.say("Time:\t\t\t\t\t $time")
-                else:
+                    await self.bot.say("Time:\t\t\t\t\t {0}" .format(time.strftime("%Y-%m-%d %H:%M"))) #2017-04-25 23:22
+                else: # catch all other non local users
                     await self.bot.say("IP:\t\t\t\t\t {0}" .format(ip_data['ip']))
                     user_loc = ip_data['loc']
                     user_geo = user_loc.split(",")
                     user_time = gettime(user_geo[0],user_geo[1])
                     user_time_data = json.loads(user_time)
                     await self.bot.say("Local Time:\t{0}" .format(user_time_data['time']))
-                usersession = users_data[b]['session']
+                usersession = users_data[b]['session'] # report user's session from the sessionid and return title TODO maybe make this a function?
                 for c in range(len(session_data)):
                     if usersession == session_data[c]['id']:
                         await self.bot.say("Session:\t\t {0}" .format(session_data[c]['title']))
                         await self.bot.say("_")
 
-    # async def drawpilesessionsizes(self):
-    #     """Get all sessions running sizes"""
-    #     basedir = "/var/drawpile/sessions"
-    #     with open("/var/drawpile/templates/drawpile.ini", "r") as a:
-    #         file = a.readlines()
-    #         a.close()
-    #     for i, line in enumerate(file):
-    #         if "sessionSizeLimit" in line:
-    #             for l in file[i:i+1]:
-    #                 sizelimit = l
-    #     await self.bot.say("Drawpile Session information")
-    #     await self.bot.say(sizelimit)
-    #     for f in os.listdir(basedir):
-    #         path = os.path.join(basedir, f)
-    #         if os.path.isfile(path):
-    #             if re.search('.dprec', path):
-    #                 size = os.path.getsize(path)
-    #                 await self.bot.say("File:\t{0}" .format(path))
-    #                 await self.bot.say("Size:\t{0}" .format(convert_size(size)))
+    async def drawpilesessionsizes(self):
+        """Get all sessions running sizes directly from server - depricated since version 1.0"""
+        basedir = "/var/drawpile/sessions"
+        with open("/var/drawpile/templates/drawpile.ini", "r") as a:
+            file = a.readlines()
+            a.close()
+        for i, line in enumerate(file):
+            if "sessionSizeLimit" in line:
+                for l in file[i:i+1]:
+                    sizelimit = l
+        await self.bot.say("Drawpile Session information (depricated)")
+        await self.bot.say(sizelimit)
+        for f in os.listdir(basedir):
+            path = os.path.join(basedir, f)
+            if os.path.isfile(path):
+                if re.search('.dprec', path):
+                    size = os.path.getsize(path)
+                    await self.bot.say("File:\t{0}" .format(path))
+                    await self.bot.say("Size:\t{0}" .format(convert_size(size)))
 
 def setup(bot):
     bot.add_cog(Drawpile(bot))
